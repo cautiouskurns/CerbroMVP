a430ade7f485107f203a4eb7020dc3cabbbd9d49
cautiouskurns <114108720+cautiouskurns@users.noreply.github.com>
2023-06-22 18:14:38 +0100

    UI Makeover

---
+++ Source/ConvaiTutorial/FolderWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FolderWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONVAITUTORIAL_API UFolderWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
