39927c4dda6c7a9056b0d83ea2a438ce197b9493
cautiouskurns <114108720+cautiouskurns@users.noreply.github.com>
2023-06-23 12:35:41 +0100

    Update UI logic

---
+++ Source/ConvaiTutorial/ImportWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ImportWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONVAITUTORIAL_API UImportWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ConvaiTutorial/InstructWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InstructWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONVAITUTORIAL_API UInstructWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
